4e8efc3472356eecdc6a4e72ba9a2cff0495ea7c
Ernest Tan <134849903+decker757@users.noreply.github.com>
2025-06-07 12:29:39 +0800

    Combined xrpl_utilities and database into my branch

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-#import xrpl_utilities
+import xrpl_utilities
 
 app = Flask(__name__)
 CORS(app)
@@ -18,23 +18,23 @@ def login():
     else:
         return jsonify({'message': 'lol'}), 401
     
-    """
+    
     if database.validate_user_login(username, password):
-            payload = {
-            'username': username,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        payload = {
+        'username': username,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
         return jsonify({'token': token})
     else:
         return jsonify({'error': 'Invalid credentials'}), 401
-    """
+    
 
-# @app.route("/api/summarize_trustlines")
-# def summarize():
-#     address = request.args.get("address")
-#     summary = xrpl_utilities.TrustLineAnalytics.summarize_trustlines(address)
-#     return jsonify(summary)
+@app.route("/api/summarize_trustlines")
+def summarize():
+    address = request.args.get("address")
+    summary = xrpl_utilities.TrustLineAnalytics.summarize_trustlines(address)
+    return jsonify(summary)
 
 if __name__ == "__main__":
     app.run(debug=True)
